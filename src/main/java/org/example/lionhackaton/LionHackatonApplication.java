4637194376067e1f9a2d4460c5491125ee5e9e90
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-06-27 15:34:07 +0900

    [ADD] 초기 세팅 추가 (Mysql DB 연결)

---
+++ src/main/java/org/example/lionhackaton/LionHackatonApplication.java
@@ -0,0 +1,13 @@
+package org.example.lionhackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionHackatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionHackatonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lionhackaton/LionHackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lionhackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionHackatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
