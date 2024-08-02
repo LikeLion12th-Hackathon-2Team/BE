7fba3752bcba19c98391f302608fb4af01f1f8cd
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-08-02 14:56:01 +0900

    [ADD] diary 저장할때 즐겨찾기, 공유여부 추가

--- src/main/java/org/example/lionhackaton/domain/dto/request/DiaryRequest.java
+++ src/main/java/org/example/lionhackaton/domain/dto/request/DiaryRequest.java
@@ -1,14 +1,18 @@
 package org.example.lionhackaton.domain.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class DiaryRequest {
 	private String diaryTitle;
 	private Long sodaIndex;
 	private String content;
 	private String purpose;
 	private Boolean isRepresentative;
+	private Boolean isFavorite;
+	private Boolean isShared;
 }
